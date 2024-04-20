4874f802e0e0c37438947d32ef9e1694035bcf77
LuckyCaves <quiroztadeo3@gmail.com>
2024-04-20 01:28:11 -0600

    Footer Added ArtCollection products added

---
+++ controllers/art_collection.js
@@ -0,0 +1,50 @@
+function createProductCard(productsContainer)
+{
+
+    let productCard = document.createElement('div');
+    productCard.id = 'Product-Card';
+
+    let cardImage = document.createElement('img');
+    cardImage.id = 'Card-Image';
+    cardImage.src = '../images/gray-default-image.jpg';
+    let blockSize = getRandomInt(15);
+    cardImage.style.blockSize = blockSize + 'em';
+
+    let cardTitle = document.createElement('h5');
+    cardTitle.id = 'Card-Title';
+    cardTitle.innerHTML = 'Title';
+
+    let cardDescription = document.createElement('p');
+    cardDescription.id = 'Card-Description';
+    cardDescription.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+
+    productCard.appendChild(cardImage);
+    productCard.appendChild(cardTitle);
+    productCard.appendChild(cardDescription);
+
+    productsContainer.appendChild(productCard);
+
+}
+
+function generateCards()
+{
+    let productsContainer = document.getElementById('Products-Container');
+    for (let i = 0; i < 12; i++)
+    {
+        let productCard = createProductCard(productsContainer);
+    }
+}
+
+function getRandomInt(max) {
+    return 5 + Math.random() * max;
+}
+
+function changePage(numero)
+{
+    let page = document.getElementById('page');
+    page.innerHTML = numero;
+}
+
+window.onload = function() {
+    generateCards();
+};
