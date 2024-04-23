ef0963b15d4b7e95dc39c0d5b065c50944844b82
carlosssfelixxx <carlosfs16@hotmail.com>
2024-04-23 13:33:48 -0700

    Added profile.html and corresponding css and js files

---
+++ controllers/profile.js
@@ -0,0 +1,59 @@
+function createProductCard(productsContainer)
+{
+
+    let productCard = document.createElement('div');
+    productCard.id = 'Product-Card';
+    productCard.addEventListener('click', function() {
+        window.location.href = 'product.html';
+    });
+
+    let cardImage = document.createElement('img');
+    cardImage.id = 'Card-Image';
+    let imageNumber = getRandomInt(5) - 4;
+    cardImage.src = '../images/image' + Math.floor(imageNumber) + '.png';
+    let blockSize = getRandomInt(15);
+    cardImage.style.blockSize = blockSize + 'em';
+
+    let cardTitle = document.createElement('h5');
+    cardTitle.id = 'Card-Title';
+    cardTitle.innerHTML = 'Title';
+
+    let cardDescription = document.createElement('p');
+    cardDescription.id = 'Card-Description';
+    cardDescription.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+
+    let deleteButton = document.createElement('button');
+    deleteButton.classList.add('btn', 'btn-secondary', 'remove-btn');
+    deleteButton.innerHTML = 'X';
+
+    productCard.appendChild(cardImage);
+    productCard.appendChild(deleteButton);
+    productCard.appendChild(cardTitle);
+    productCard.appendChild(cardDescription);
+
+    productsContainer.appendChild(productCard);
+
+}
+
+function generateCards()
+{
+    let productsContainer = document.getElementById('Artist-Product');
+    for (let i = 0; i < 12; i++)
+    {
+        let productCard = createProductCard(productsContainer);
+    }
+}
+
+function getRandomInt(max) {
+    return 5 + Math.random() * max;
+}
+
+function changePage(numero)
+{
+    let page = document.getElementById('page');
+    page.innerHTML = numero;
+}
+
+window.onload = function() {
+    generateCards();
+};
