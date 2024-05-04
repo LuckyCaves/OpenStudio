720e6a993fe2030433f312f080837c8035a02935
Isaac <isaac.vazquez@iteso.mx>
2024-05-04 11:01:23 -0600

    Undo server.js changes

--- server.js
+++ server.js
@@ -1,39 +1,15 @@
-require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const path = require('path');
-
 const router = require ('./app/controllers/router.js');
 
 const app = express();
-const port = process.env.PORT || 3000;
-
-// BodyParser Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
+const port = 3000;
 
 app.use(express.json());
 app.use('/', router);
 app.use(express.static('app'));
 app.use('app/web/', express.static('web'));
 
-// Express session
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-  }));
-  
-
-// Connect to MongoDB
-mongoose.connect(process.env.DB_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
-
-app.use('/', router);
-
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log("Aplicacion de ejemplo corriendo en puerto " + port);
 });
