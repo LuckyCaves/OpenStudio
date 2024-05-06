79b20c9d9ba2fb48068a09fef184244aee6c8e4b
Isaac <isaac.vazquez@iteso.mx>
2024-05-06 09:40:43 -0600

    Added Style to Searchbar result

--- app/web/controllers/search.js
+++ app/web/controllers/search.js
@@ -32,7 +32,7 @@ function fillInfo(products) {
     if (productsContainer) {
         productsContainer.innerHTML = ''; // Clear previous results
         products.forEach(product => {
-            const productDiv = document.createElement('div');
+            /*const productDiv = document.createElement('div');
             productDiv.className = 'product';
             productDiv.innerHTML = `
                 <h3>${product.title}</h3>
@@ -40,9 +40,40 @@ function fillInfo(products) {
                 <img src="${product.imageUrl}" alt="${product.title}" style="width:100px; height:auto;">
                 <p>Price: $${product.price}</p>
             `;
-            productsContainer.appendChild(productDiv);
+            productsContainer.appendChild(productDiv);*/
+            createProductCard(productsContainer, product);
         });
     } else {
         console.error('Products-Container element not found!');
     }
+}
+
+function createProductCard(productsContainer, product)
+{
+
+    let productCard = document.createElement('div');
+    productCard.id = 'Product-Card';
+    productCard.addEventListener('click', function() {
+        localStorage.setItem('product', product.sku);
+        window.location.href = 'product';
+    });
+
+    let cardImage = document.createElement('img');
+    cardImage.id = 'Card-Image';
+    cardImage.src = product.image;
+
+    let cardTitle = document.createElement('h5');
+    cardTitle.id = 'Card-Title';
+    cardTitle.innerHTML = product.title;
+
+    let cardDescription = document.createElement('p');
+    cardDescription.id = 'Card-Description';
+    cardDescription.innerHTML = '$' + product.price;
+
+    productCard.appendChild(cardImage);
+    productCard.appendChild(cardTitle);
+    productCard.appendChild(cardDescription);
+
+    productsContainer.appendChild(productCard);
+
 }
